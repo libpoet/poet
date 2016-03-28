f2393c7a6c51ace2815583be5403468efea8e065
Connor Imes <connor.k.imes@gmail.com>
2016-03-28 15:12:53 -0500

    Use heartbeats-simple and energymon in tests which we can conditionally check for

--- test/double_loop_test.c
+++ test/double_loop_test.c
@@ -1,18 +1,39 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
-#include <heartbeats/heartbeat-accuracy-power.h>
+#include <heartbeat-pow.h>
+#include <energymon-default.h>
 #include "poet.h"
 #include "poet_config.h"
 #include "poet_math.h"
 
-const char* HB_LOG_FILE = "/tmp/heartbeat_log.txt";
 const char* POET_LOG_FILE = "/tmp/poet_log.txt";
-heartbeat_t* heart;
 
 int BIG_NUM1;
 int BIG_NUM2_START = 10000000;
 int BIG_NUM2 = 10000000;
+
+static inline uint64_t get_time(void) {
+  struct timespec ts;
+#ifdef __MACH__
+  // OS X does not have clock_gettime, use clock_get_time
+  clock_serv_t cclock;
+  mach_timespec_t mts;
+  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+  clock_get_time(cclock, &mts);
+  mach_port_deallocate(mach_task_self(), cclock);
+  ts.tv_sec = mts.tv_sec;
+  ts.tv_nsec = mts.tv_nsec;
+#else
+  // CLOCK_REALTIME is always supported, this should never fail
+  clock_gettime(CLOCK_REALTIME, &ts);
+#endif
+  // must use a const or cast a literal - using a simple literal can overflow!
+  const uint64_t ONE_BILLION = 1000000000;
+  return ts.tv_sec * ONE_BILLION + ts.tv_nsec;
+}
 
 int main(int argc, char** argv) {
   if ( argc != 4 ) {
@@ -21,18 +42,22 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  if(getenv("HEARTBEAT_ENABLED_DIR") == NULL) {
-    fprintf(stderr, "ERROR: need to define environment variable HEARTBEAT_ENABLED_DIR (see README)\n");
+  uint64_t window_size = atoi(argv[3]);
+  heartbeat_pow_context hb;
+  heartbeat_pow_record* hb_window_buffer = malloc(window_size * sizeof(heartbeat_pow_record));
+  if (hb_window_buffer == NULL) {
+    perror("malloc");
     return 1;
   }
-
-  heart = heartbeat_init(atoi(argv[3]),
-                         atoi(argv[1]),
-                         HB_LOG_FILE,
-                         CONST(atof(argv[2])),
-                         CONST(atof(argv[2])));
-  if (heart == NULL) {
-    fprintf(stderr, "Failed to initialize heartbeat\n");
+  int hb_fd = 1;
+  if (heartbeat_pow_init(&hb, window_size, hb_window_buffer, hb_fd, NULL)) {
+    perror("Failed to initialize heartbeat");
+    return 1;
+  }
+  hb_pow_log_header(hb_fd);
+  energymon em;
+  if (energymon_get_default(&em) || em.finit(&em)) {
+    perror("Failed to initialize energymon");
     return 1;
   }
 
@@ -40,7 +65,7 @@ int main(int argc, char** argv) {
   BIG_NUM1 = atoi(argv[1]);
   unsigned int s_nstates;
   poet_control_state_t * s_control_states;
-  get_control_states("config/default/control_config",
+  get_control_states("../config/default/control_config",
                      &s_control_states,
                      &s_nstates);
   poet_state * state = poet_init(CONST(atof(argv[2])),
@@ -54,23 +79,29 @@ int main(int argc, char** argv) {
   }
 
   int i, j;
-  heartbeat_record_t hbr;
+  uint64_t time_start, time_end, energy_start, energy_end;
+  time_end = get_time();
+  energy_end = em.fread(&em);
   for (i = 0; i < BIG_NUM1; i++) {
-    heartbeat(heart, i);
-    hb_get_current(heart, &hbr);
-    real_t hbr_window_rate = hbr_get_window_rate(&hbr);
-    real_t hbr_window_power = hbr_get_window_power(&hbr);
-    poet_apply_control(state, i, hbr_window_rate, hbr_window_power);
-
+    time_start = time_end;
+    energy_start = energy_end;
     for (j = 0; j < BIG_NUM2; j++) {
       dummy = dummy >> 1;
       dummy = dummy - 1;
     }
+    time_end = get_time();
+    energy_end = em.fread(&em);
+    heartbeat_pow(&hb, i, 1, time_start, time_end, energy_start, energy_end);
+    real_t hb_window_perf = hb_pow_get_window_perf(&hb);
+    real_t hb_window_power = hb_pow_get_window_power(&hb);
+    poet_apply_control(state, i, hb_window_perf, hb_window_power);
   }
 
   poet_destroy(state);
   free(s_control_states);
-  heartbeat_finish(heart);
+  hb_pow_log_window_buffer(&hb, hb_pow_get_log_fd(&hb));
+  free(hb_window_buffer);
+  em.ffinish(&em);
 
   return 0;
 }
--- test/processor_speed_test.c
+++ test/processor_speed_test.c
@@ -1,7 +1,10 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
-#include <heartbeats/heartbeat-accuracy-power.h>
+#include <heartbeat-pow.h>
+#include <energymon-default.h>
 #include "poet.h"
 #include "poet_config.h"
 #include "poet_math.h"
@@ -18,9 +21,7 @@
 { 2 , 1200000 , 1 } , \
 { 3 , 1300000 , 1 }}
 
-const char* HB_LOG_FILE = "/tmp/heartbeat_log.txt";
 const char* POET_LOG_FILE = "/tmp/poet_log.txt";
-heartbeat_t* heart;
 
 int BIG_NUM1;
 int BIG_NUM2_START = 10000000;
@@ -67,6 +68,26 @@ void apply2(void * states,
   }
 }
 
+static inline uint64_t get_time(void) {
+  struct timespec ts;
+#ifdef __MACH__
+  // OS X does not have clock_gettime, use clock_get_time
+  clock_serv_t cclock;
+  mach_timespec_t mts;
+  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+  clock_get_time(cclock, &mts);
+  mach_port_deallocate(mach_task_self(), cclock);
+  ts.tv_sec = mts.tv_sec;
+  ts.tv_nsec = mts.tv_nsec;
+#else
+  // CLOCK_REALTIME is always supported, this should never fail
+  clock_gettime(CLOCK_REALTIME, &ts);
+#endif
+  // must use a const or cast a literal - using a simple literal can overflow!
+  const uint64_t ONE_BILLION = 1000000000;
+  return ts.tv_sec * ONE_BILLION + ts.tv_nsec;
+}
+
 
 int main(int argc, char** argv) {
   if ( argc != 4 ) {
@@ -75,18 +96,22 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  if(getenv("HEARTBEAT_ENABLED_DIR") == NULL) {
-    fprintf(stderr, "ERROR: need to define environment variable HEARTBEAT_ENABLED_DIR (see README)\n");
+  uint64_t window_size = atoi(argv[3]);
+  heartbeat_pow_context hb;
+  heartbeat_pow_record* hb_window_buffer = malloc(window_size * sizeof(heartbeat_pow_record));
+  if (hb_window_buffer == NULL) {
+    perror("malloc");
     return 1;
   }
-
-  heart = heartbeat_init(atoi(argv[3]),
-                         atoi(argv[1]),
-                         HB_LOG_FILE,
-                         CONST(atof(argv[2])),
-                         CONST(atof(argv[2])));
-  if (heart == NULL) {
-    fprintf(stderr, "Failed to initialize heartbeat\n");
+  int hb_fd = 1;
+  if (heartbeat_pow_init(&hb, window_size, hb_window_buffer, hb_fd, NULL)) {
+    perror("Failed to initialize heartbeat");
+    return 1;
+  }
+  hb_pow_log_header(hb_fd);
+  energymon em;
+  if (energymon_get_default(&em) || em.finit(&em)) {
+    perror("Failed to initialize energymon");
     return 1;
   }
 
@@ -107,22 +132,28 @@ int main(int argc, char** argv) {
   }
 
   int i, j;
-  heartbeat_record_t hbr;
+  uint64_t time_start, time_end, energy_start, energy_end;
+  time_end = get_time();
+  energy_end = em.fread(&em);
   for (i = 0; i < BIG_NUM1; i++) {
-    heartbeat(heart, i);
-    hb_get_current(heart, &hbr);
-    real_t hbr_window_rate = hbr_get_window_rate(&hbr);
-    real_t hbr_window_power = hbr_get_window_power(&hbr);
-    poet_apply_control(state, i, hbr_window_rate, hbr_window_power);
-
+    time_start = time_end;
+    energy_start = energy_end;
     for (j = 0; j < BIG_NUM2; j++) {
       dummy = dummy >> 1;
       dummy = dummy - 1;
     }
+    time_end = get_time();
+    energy_end = em.fread(&em);
+    heartbeat_pow(&hb, i, 1, time_start, time_end, energy_start, energy_end);
+    real_t hb_window_perf = hb_pow_get_window_perf(&hb);
+    real_t hb_window_power = hb_pow_get_window_power(&hb);
+    poet_apply_control(state, i, hb_window_perf, hb_window_power);
   }
 
   poet_destroy(state);
-  heartbeat_finish(heart);
+  hb_pow_log_window_buffer(&hb, hb_pow_get_log_fd(&hb));
+  free(hb_window_buffer);
+  em.ffinish(&em);
 
   return 0;
 }
